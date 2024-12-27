9484fc5f4415275ab0175c5d79a27c95a7b69e74
Vivek Lode <vivek.lode1@gmail.com>
2024-12-27 17:39:11 +0530

    Array Sorting And Factorial
    Array Sorting And Factorial added

--- DSA_JS/array_sorting.php
+++ DSA_JS/array_sorting.php
@@ -6,30 +6,24 @@
     <title>Array Sorting</title>
 </head>
 <body>
-<!-- <h2>Array Sorting</h2> -->
-<h3>Original Array: </h3>
-<?php
-    $arr = array(5, 3, 7, 1, 9, 2, 8, 4, 6);
-    echo "<pre>";
-    print_r($arr);
-    echo "</pre>";
-?>
+<h2>Array Sorting</h2>
+<h3>Original Array: (5, 3, 7, 1, 9, 2, 8, 4, 6) </h3>
 
 <script>
     var arr = [5, 3, 7, 1, 9, 2, 8, 4, 6];
     document.write("<h2>Sorted Array: </h2>");
 
-    for( let i =0; i < arr.length; i++){
-        for( let j = 0; j < arr.length; j++){
-            if(arr[i] < arr[j]){
-                let temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }   
-    }
-    document.write(arr);
-    // document.write(arr.sort(function(a, b){return a-b;}));
+    // for( let i =0; i < arr.length; i++){
+    //     for( let j = 0; j < arr.length; j++){
+    //         if(arr[i] < arr[j]){
+    //             let temp = arr[i];
+    //             arr[i] = arr[j];
+    //             arr[j] = temp;
+    //         }
+    //     }   
+    // }
+    // document.write(arr);
+    document.write(arr.sort(function(a, b){return a-b;}));
 </script>
 </body>
 </html>
